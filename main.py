5947cb96edc9f3a7fbca844daa69b10e0c877dbc
matteosz <mattsuez01@gmail.com>
2024-05-13 14:15:25 +0200

    Fixed state in update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from copy import copy
 import torch
 from torch import multiprocessing as mp
 import gymnasium as gym
@@ -6,30 +7,32 @@ from model import A2C
 import json
 
 ENV_NAME = 'CartPole-v1'
-MAX_STEPS = 5e5
-EVAL_STEPS = 2e4
-LOG_STEPS = 1e3
+MAX_STEPS = int(5e5)
+EVAL_STEPS = int(2e4)
+LOG_STEPS = int(1e3)
 EVAL_EPISODES = 10
 
-AGENTS = [2, 42, 82]
-CURRENT_AGENT = 0
+AGENTS = [2, 42, 242]
+
+DEBUG = False
 
 class Data:
-    def __init__(self):
+    def __init__(self, agent):
+        self.agent = agent
         self.logs = ""
         self.plots = defaultdict(list)
 
     def append(self, log):  
         self.logs += log
-        if len(self.logs) > 1e3:
-            with open(f'logs_{CURRENT_AGENT}.txt', 'a+') as f:
+        if len(self.logs) > 1e4:
+            with open(f'logs_{self.agent}.txt', 'a+') as f:
                 f.write(self.logs)
             self.logs = ""
 
     def log(self):
-        with open(f'logs_{CURRENT_AGENT}.txt', 'a+') as f:
+        with open(f'logs_{self.agent}.txt', 'a+') as f:
             f.write(self.logs)
-        with open(f'plots_{CURRENT_AGENT}.json', 'w') as t:
+        with open(f'plots_{self.agent}.json', 'w') as t:
             json.dump(self.plots, t)
 
 def collect_data(env, model, state, n):
@@ -44,44 +47,41 @@ def collect_data(env, model, state, n):
         state, reward, terminated, truncated, _ = env.step(action)
 
         tot_reward += reward
-        discounted_reward += model.gamma ** i * float(reward)
+        discounted_reward += model.gamma ** i * reward
 
         i += 1
-        if truncated:
+        done = terminated or truncated
+        if done:
             break
         
     discounted_reward += model.gamma ** i * model.get_reward(state) * (1 - terminated)
       
-    return state, first_action, tot_reward, discounted_reward, truncated or terminated
+    return state, first_action, tot_reward, discounted_reward, done
 
-def evaluate(model, data, last=False):
-    if last:
-        env = gym.make(ENV_NAME, render_mode='human')
-    else:
-        env = gym.make(ENV_NAME)
+def evaluate(model, data, render=False):
+    env = gym.make(ENV_NAME, render_mode='human' if render else None)
     episode_rewards = .0
 
     states = []
     values = []
-
     for i in range(EVAL_EPISODES):
         state, _ = env.reset()
         done = False
 
         while not done:
-            best_action = model.get_best_action(state)
-            state, reward, terminated, truncated, _ = env.step(best_action)
-            episode_rewards += reward
-            done = terminated or truncated
-            #if last:
-            #    env.render()
+            with torch.no_grad():
+                best_action = model.get_best_action(state)
+                state, reward, terminated, truncated, _ = env.step(best_action)
+                episode_rewards += reward
+                done = terminated or truncated
+                if render:
+                    env.render()
 
-            if i == EVAL_EPISODES - 1:
-                states.append(state)
-                values.append(model.get_reward(state).detach().item())
+                if i == EVAL_EPISODES - 1:
+                    states.append(state)
+                    values.append(model.get_reward(state).item())
 
     env.close()
-
     data.plots['3'].append([states, values])
 
     return episode_rewards / EVAL_EPISODES
@@ -89,22 +89,23 @@ def evaluate(model, data, last=False):
 def run_episode(env, model, n, iterations, lock, current_reward, n_episodes, data):
     state, _ = env.reset()
     tot_reward = .0
-    done = False
 
-    while not done and iterations.value <= MAX_STEPS:
-        freezed_model = model.copy()
-        state, action, reward, discounted_reward, done = collect_data(env, freezed_model, state, n)
+    while iterations.value <= MAX_STEPS:
+        with torch.no_grad():
+            next_state, action, reward, discounted_reward, done = collect_data(env, copy(model), state, n)
         tot_reward += reward
 
         with lock:
             actor_loss, critic_loss = model.update_step(state, action, discounted_reward)
         
             if iterations.value % LOG_STEPS == 0:
-                append_log = ""
-                append_log += f"Iteration {iterations.value}:\n\tActor loss = {actor_loss}\n\tCritic loss = {critic_loss}\n"
-                append_log += f"Average episodic rewards = {current_reward / n_episodes:.2f}\n"
+                append_log = f"Iteration {iterations.value}:\n"
+                append_log += f"\tActor loss = {actor_loss}\n\tCritic loss = {critic_loss}\n"
+                append_log += f"Current average episodic rewards = {current_reward / n_episodes:.2f}\n"
+                
                 data.plots['1'].append([n_episodes, current_reward / n_episodes])
                 data.plots['4'].append([actor_loss, critic_loss])
+                
                 if iterations.value % EVAL_STEPS == 0:
                     eval_reward = evaluate(model, data)
                     append_log += f'Average evaluation episodic reward = {eval_reward:.2f}\n'
@@ -115,26 +116,34 @@ def run_episode(env, model, n, iterations, lock, current_reward, n_episodes, dat
                 data.append(append_log)
             
             iterations.value += n
+        
+        if done:
+            break
+        state = next_state
 
     return tot_reward
 
 def worker_fn(idx, envs, model, n, iterations, lock, data):
     episodes, tot_reward = 0, .0
     while iterations.value <= MAX_STEPS:
-        tot_reward += run_episode(envs[idx], model, n, iterations, lock, tot_reward, episodes, data)
+        r = run_episode(envs[idx], model, n, iterations, lock, tot_reward, episodes, data)
+        tot_reward += r
         episodes += 1
+        if DEBUG:
+            print(f'#{episodes:06d} - {r:.2f}')
     
+    # Log the data and evaluate the model one last time (only 1 worker performs this)
     with lock:
         if iterations.value < 2 * MAX_STEPS:
-            evaluate(model, data, last=True)
-        iterations.value = 2 * int(MAX_STEPS)
+            evaluate(model, data, render=True)
+            data.log()
 
-    data.log()
+            iterations.value = 3 * int(MAX_STEPS)
 
-def a2c(k=1, n=1, agent=0, data=Data()):
-    CURRENT_AGENT = agent
-    torch.manual_seed(AGENTS[CURRENT_AGENT])
-    print(f'Running A2C with {k} workers and {n} steps (Agent {CURRENT_AGENT})...')
+def a2c(k=1, n=1, agent=0):
+    torch.manual_seed(agent)
+    data = Data(agent)
+    print(f'Running A2C with {k} workers and {n} steps (Agent #{agent})...')
 
     envs = [gym.make(ENV_NAME)] * k
     num_actions, num_states = envs[0].action_space.n, envs[0].observation_space.shape[0]
--- model.py
+++ model.py
@@ -8,6 +8,7 @@ def load_hyperparameters(filename='hyperparams.json'):
     with open(filename, 'r') as f:
         return json.load(f)
     
+HYPERPARAMS = load_hyperparameters()
 DEVICE = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
 class A2C():
@@ -20,18 +21,14 @@ class A2C():
         self.actor = Actor(input_size, output_size, hidden_size, activation)
         self.critic = Critic(input_size, 1, hidden_size, activation)
 
-        self.actor.share_memory()
-        self.critic.share_memory()
         self.actor.to(DEVICE)
         self.critic.to(DEVICE)
 
-        hyperparameters = load_hyperparameters()
+        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=HYPERPARAMS['actor_lr'])
+        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=HYPERPARAMS['critic_lr'])
+        self.gamma = HYPERPARAMS['gamma']
 
-        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=hyperparameters['actor_lr'])
-        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=hyperparameters['critic_lr'])
-        self.gamma = hyperparameters['gamma']
-
-    def copy(self):
+    def __copy__(self):
         new_model = A2C(self.input_size, self.output_size, self.hidden_size, self.activation)
         new_model.actor.load_state_dict(self.actor.state_dict())
         new_model.critic.load_state_dict(self.critic.state_dict())
@@ -42,23 +39,21 @@ class A2C():
         return self.actor(state)
 
     def sample_action(self, state):
-        return torch.multinomial(self.__get_action_probs(state), 1).detach().item()
+        return torch.multinomial(self.__get_action_probs(state).detach(), 1).item()
     
     def get_best_action(self, state):
-        return torch.argmax(self.__get_action_probs(state)).detach().item()
+        return torch.argmax(self.__get_action_probs(state).detach()).item()
     
     def get_reward(self, state):
         state = torch.tensor(state, dtype=torch.float32).to(DEVICE)
         return self.critic(state)
 
     def update_step(self, state, action, discount_reward):
-        advantage = discount_reward - self.get_reward(state)
-
-        actor_loss = -torch.log(self.__get_action_probs(state)[action]) * advantage
-        critic_loss = advantage ** 2
-
         self.actor_optimizer.zero_grad()
         self.critic_optimizer.zero_grad()
+
+        actor_loss = torch.log(self.__get_action_probs(state)[action]) * (self.get_reward(state) - discount_reward)
+        critic_loss = (discount_reward - self.get_reward(state)) ** 2
         
         actor_loss.backward(retain_graph=True)
         critic_loss.backward()
